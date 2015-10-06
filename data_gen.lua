841aaf9b18469b385bf1a82cd3fec1e1c09cde5d
vic-w <vic.wu@hotmail.com>
2015-10-07 01:11:04 +0800

    generate data

---
+++ data_gen.lua
@@ -0,0 +1,46 @@
+require 'torch'
+require 'gnuplot'
+
+math.randomseed(1)
+
+sample_length = 10000
+
+X = torch.zeros(sample_length)
+Y = torch.zeros(sample_length)
+
+for n=2,10 do
+  sample_count = 0
+
+  input = {}
+  output = {}
+
+  while sample_count < sample_length do
+    t_blank = math.random(1,10)
+    t_intense = math.random(1,10)
+    for i = 1,t_blank do
+      table.insert(input, 0)
+      table.insert(output, 0)
+    end
+    table.insert(input, t_intense)
+    table.insert(output, 0)
+    for i = 1,t_intense do
+      table.insert(input, 0)
+      table.insert(output, 1)
+    end
+    sample_count = sample_count+t_blank+t_intense+1
+  end
+
+  input = torch.Tensor(input)
+  input = input[{{1, sample_length}}]
+  output = torch.Tensor(output)
+  output = output[{{1, sample_length}}]
+  X = X:cat(input,2)
+  Y = Y:cat(output,2)
+end
+
+torch.save('data.t7', {X, Y})
+
+--gnuplot.pngfigure('img/timer.png')
+--gnuplot.plot({input},{output})
+--gnuplot.plotflush()
+
